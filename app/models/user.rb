b4b28abe4ddcafb81ae2b69597a0764484a0a6ea
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2023-03-12 00:28:22 +0100

    model user presence true deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  has_one_attached :profile_pic, presence: true
+  has_one_attached :profile_pic
 end
