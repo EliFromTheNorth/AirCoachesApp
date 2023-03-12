4f11adb00d97e6e74d8ccbc90894f0cdfb3034c2
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2023-03-13 00:51:09 +0100

    login btn in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :coaching_offers
   has_many :bookings
-  # has_many :bookings, through: :coaching_offers
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_pic, presence: true
 
   has_one_attached :profile_pic
+
+  # validates_attachment :profile_pic, :content_type => { :content_type => /\Aimage\/jpg/ }
 end
