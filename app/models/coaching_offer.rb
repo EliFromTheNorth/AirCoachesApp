7a04a3e51d9ebb73562e3b50032d1f61e30cd2e4
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2023-03-12 01:56:33 +0100

    card title truncate and character limits

--- app/models/coaching_offer.rb
+++ app/models/coaching_offer.rb
@@ -9,7 +9,8 @@ class CoachingOffer < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
-  validates :description, length: { minimum: 30 }
+  validates :title, length: { minimum: 5, maximum: 30 }
+  validates :description, length: { minimum: 20, maximum: 400 }
   validates :price, presence: true
   validates :user, presence: true
   validates :skill, inclusion: { in: ["Ruby", "Rails", "JavaScript", "HTML", "CSS", "FrontEnd", "BackEnd", "Python", "PHP"] }
