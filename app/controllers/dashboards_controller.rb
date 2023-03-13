80ef93cd16be85e521d1df44e0d0b5e25facc2d3
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2023-03-13 01:58:16 +0100

    3rd tab your offers, css missing

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,6 @@ class DashboardsController < ApplicationController
   def dashboard
     @coachee_bookings = current_user.bookings
     @coach_bookings = current_user.coaching_offers.extract_associated(:bookings).flatten
+    @your_offers = current_user.coaching_offers
   end
 end
